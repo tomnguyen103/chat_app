7e76f4a1d41f885e798fb004c1ee4176aaa29734
Tom Nguyen <huuthong103@gmail.com>
2021-02-01 00:52:43 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID = "3de919c0-82fa-4cc5-92fc-469010c9d274"
+            userName = "tomnguyen11"
+            userSecret = "123123"
+        />
+    );
+};
+
+export default App;
